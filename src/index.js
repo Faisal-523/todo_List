48161bff59ab9f32e3ad0a04852042d49200fd7b
Faisal-523 <faisalbukhari523@gmail.com>
2020-12-30 16:07:25 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {addProject,getActiveProject,createTask,displayProjects,displayTasks} from './modules/factory_logic.js'
+
+addProject('new Project1', 'Test run1');
+createTask('new task1', 'Test Task1', '31/12/2020');
+createTask('new task2', 'Test Task2', '31/12/2020');
+
+displayProjects();
+
+addProject('new Project2', 'Test run2');
+createTask('Project2 task1', 'Project2 Test Task1','31/12/2020');
+createTask('Project2 task2', 'Project2 Test Task2', '31/12/2020');
+
+displayTasks();
---
+++ src/modules/factory_logic.js
@@ -0,0 +1,40 @@
+
+    let allProjects = [];
+
+    const addProject = (name, description) =>{
+        clearActiveProjects();
+        allProjects.push({
+            name,
+            description,
+            todoList:[],
+            active:true,
+        });
+    };
+
+    const clearActiveProjects = ()=>{
+        allProjects.forEach(project => project.active = false);
+    };
+
+    const getActiveProject = ()=>{
+        let activeArray =  allProjects.filter(project => project.active == true);
+        return activeArray[0];
+    };
+
+    const createTask = function(name, description, dueDate){
+        getActiveProject().todoList.push({
+            name,
+            description,
+            dueDate,
+        });
+    };
+
+    const displayProjects = ()=>{
+        allProjects.forEach(project => console.log(project));
+    }
+
+    const displayTasks = ()=>{
+        getActiveProject().todoList.forEach(item=>console.log(item));
+    }
+
+    export {addProject,getActiveProject,createTask,displayProjects,displayTasks};
+
