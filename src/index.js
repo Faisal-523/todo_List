336d228756957b56c66d7de7b86020ff434ed120
Faisal-523 <faisalbukhari523@gmail.com>
2021-01-01 14:57:39 +0530

    update for task count

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ createTask('new task2', 'Test Task2', '31/12/2020');
 addProject('new Project2', 'Test run2');
 createTask('Project2 task1', 'Project2 Test Task1','31/12/2020');
 createTask('Project2 task2', 'Project2 Test Task2', '31/12/2020');
+createTask('Project2 task3', 'Project2 Test Task3','31/12/2020');
+createTask('Project2 task4', 'Project2 Test Task4', '31/12/2020');
 
 displayProjects();
 showProjects();
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,9 +1,19 @@
 import {allProjects, getActiveProject} from './factory_logic.js'
 
+const taskContainer = document.querySelector('.tasks');
+const projectList = document.querySelector('.project-list');
+
+taskContainer.addEventListener('click',(e)=>{
+    if(e.target.tagName.toLowerCase() === 'input'){
+       getActiveProject().todoList[Number(e.target.dataset.value)].completion = e.target.checked;
+       console.log(e);
+    }
+    showTaskCount();
+});
+
 
 function showProjects(){
 
-    let projectList = document.querySelector('.project-list');
     allProjects.forEach((project, i)=>{
         let list = document.createElement('li');
         list.dataset.value = i;
@@ -16,17 +26,28 @@ function showProjects(){
 
 function showTasks(){
     let templateTask = document.getElementById('task');
-    let taskContainer = document.querySelector('.tasks');
-    getActiveProject().todoList.forEach((item,i)=>{
+    let listTitle = document.querySelector('.list-title');
+    let currentProject = getActiveProject();
+    currentProject.todoList.forEach((item,i)=>{
         let taskElement = document.importNode(templateTask.content,true);
         let checkbox = taskElement.querySelector('input');
         checkbox.id = i;
         checkbox.checked = item.completion;
+        checkbox.dataset.value = i;
         let label = taskElement.querySelector('label');
         label.htmlFor = i
         label.append(item.name);
         taskContainer.appendChild(taskElement);
     });
+    listTitle.textContent = `${currentProject.name} tasks`;
+    showTaskCount();
+
 };
+
+function showTaskCount(){
+    let taskCount = document.querySelector('.task-count');
+    let count = getActiveProject().todoList.filter(item => item.completion!=true).length;
+    taskCount.textContent = `${count} tasks pending`;
+}
 
 export {showProjects, showTasks};
